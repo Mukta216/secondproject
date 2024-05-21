e8727e36312e98f05b8e0874161a0ec88777b51e
teamleader <tl@gmail.com>
2024-05-21 04:52:07 -0700

    Hey I am mitali and I am pushing this Frist Java Project without changes

---
+++ FirstJava/src/com/codeo/newpack/Myproj.java
@@ -0,0 +1,13 @@
+package com.codeo.newpack;
+
+import com.cg.accessmodifier.InTermsOfVariable;
+
+public class Myproj {
+
+	public static void main(String[] args) {
+		InTermsOfVariable itov=new InTermsOfVariable();
+		System.out.println(itov.i);
+
+	}
+
+}
